81f353821ecaeeac92948fcf4408efc99a7b33c7
Rico Moorer <rico@alumni.upenn.edu>
2015-10-14 20:56:32 -0700

    Rico's shit

---
+++ main.js
@@ -0,0 +1,21 @@
+curl -X POST --data-urlencode 'payload={"channel": "#finance", "username": "webhookbot", "text": "This is posted to #finance and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+
+
+var channelName = '#finance';
+var userName = 'helpbot';
+var messageText;
+
+var userName1 = null;
+var userName2 = null;
+
+
+
+payload.channel = channelName;
+payload.username = userName;
+payload.text = text;
+
+
+
+
+
+var payload = {text: messageText + };
