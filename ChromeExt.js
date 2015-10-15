e1cc49db3b74d9971acd886ec94074170903cd24
mlee277 <mlee277@gmail.com>
2015-10-14 21:54:02 -0700

    as of 9:53pm

--- ChromeExt.js
+++ ChromeExt.js
@@ -1,1 +1,16 @@
-alert("hello world");
+function showIndex() {
+       var index_url = "http://www.eyebleach.me/kittens/";
+       chrome.tabs.create({
+       url: index_url
+    });
+ }
+
+var doc = document.getElementById('#index');
+
+addEventListener("click", showIndex);
+
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     chrome.tabs.create({
+//         url: "http://google.com"
+//     });
+// });
